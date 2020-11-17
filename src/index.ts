0d507242511a2f723c3458a8179ff93ca5f427ed
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-17 22:08:22 +0000

    Initial Commit: Node/TypeScript Setup & MikroORM Setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { MikroORM } from '@mikro-orm/core'
+import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
+
+
+
+const main = async () => {
+  console.log('dirname', __dirname)
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+  // const post = orm.em.create(Post, {title: 'My first post'})
+  // await orm.em.persistAndFlush(post)
+
+  const posts = await orm.em.find(Post, {})
+
+  console.log(posts)
+}
+
+
+main().catch(err => {
+  console.error(err)
+})
