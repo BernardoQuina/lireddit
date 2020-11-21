38ba2213de9480f1e24b1fa275528940b828ba02
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-21 21:18:05 +0000

    Chakra cleanup & register page setup

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,13 +1,53 @@
 import React from 'react'
+import { Formik, Form } from 'formik'
+import { Box, Button } from '@chakra-ui/react'
+import { Wrapper } from '../components/Wrapper'
+import { InputField } from '../components/InputField'
 
 interface registerProps {
 
 }
 
+
+
 const Register: React.FC<registerProps> = ({}) => {
-    return (
-      <div>register page</div>
-    )
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name='username'
+              placeholder='username'
+              label='Username'
+            />
+            <Box mt={4}>
+              <InputField
+                name='password'
+                placeholder='password'
+                label='Password'
+                type='password'
+              />
+            </Box>
+            <Button
+              type='submit'
+              isLoading={ isSubmitting }
+              mt={4}
+              colorScheme='teal'
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+    
+  )
 }
 
 export default Register
