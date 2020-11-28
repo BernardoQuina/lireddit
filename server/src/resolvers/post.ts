7b66ae3d021826596d4c352858082f1d0568fd46
Bernardo-Quina <bernardoquina@gmail.com>
2020-11-28 16:46:27 +0000

    Auth Middleware & Many to One Relationship: Posts => User

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,15 @@
 import { Post } from '../entities/Post'
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql'
+import { MyContext } from '../types'
+import { isAuth } from '../utils/middleware/isAuth'
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string
+  @Field()
+  text: string
+}
 
 @Resolver()
 export class PostResolver {
@@ -14,9 +24,12 @@ export class PostResolver {
   }
 
   @Mutation(() => Post)
-  async createPost(@Arg('title') title: string): Promise<Post> {
-    // 2 sql queries
-    return Post.create({ title }).save()
+  @UseMiddleware(isAuth) // If not logged in this middleware will throw an error
+  async createPost(
+    @Arg('input') input: PostInput,
+    @Ctx() { req }: MyContext
+  ): Promise<Post> {
+    return Post.create({ ...input, creatorId: req.session.userId  }).save()
   }
 
   @Mutation(() => Post, { nullable: true })
